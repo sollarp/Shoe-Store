7804040bf3dd6ea7a537abb239a3606898475cfd
sollarp <szpitor@gmail.com>
2022-04-22 21:13:42 +0100

    first commit

---
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    //Inflating and Returning the View with DataBindingUtil
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+            R.layout.fragment_login,container,false)
+
+
+        return binding.root
+    }
+
+}
